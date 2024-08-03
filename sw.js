eb657fb7f1fadc8259a2b013c6c966dd7f191461
sable-emperor <303454270@qq.com>
2024-08-03 21:36:02 +0800

    Site updated: 2024-08-03 21:36:02

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722691915015";
+const VERSION = "1722692142623";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
